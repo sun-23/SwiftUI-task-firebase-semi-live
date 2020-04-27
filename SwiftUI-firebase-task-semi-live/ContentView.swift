e544b85ef755122b8d6a7510892267e22f64ddbb
sun-23 <twinsamson@gmail.com>
2020-04-27 11:58:58 +0700

    Initial Commit

---
+++ SwiftUI-firebase-task-semi-live/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-firebase-task-semi-live
+//
+//  Created by sun on 27/4/2563 BE.
+//  Copyright © 2563 sun. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
